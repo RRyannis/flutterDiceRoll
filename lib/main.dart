1e64fdf49551ae4bcb8baa8026a933c70bfb46fc
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-02-07 18:52:21 +0200

    adding color

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         body: Center(
           child: Text('Hello, World!'),
         ),
